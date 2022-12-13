08e76f0707ffe75647bfaea1413b7ca2960c4ea1
Sean <sean_shen@quantatw.com>
2022-12-13 15:15:52 +0800

    Deleted the file from the git repository

--- MakeOnlyNumbersPwdDictionary.py
+++
@@ -1,8 +0,0 @@
-import itertools as its
-numbers = "0123456789"
-r = its.product(numbers,repeat=8)
-dic = open("NumbersPwd.txt","a")
-for i in r:
-    dic.write("".join(i))
-    dic.write("".join("\n"))
-dic.close()
--- MakePwdDictionary.py
+++
@@ -1,9 +0,0 @@
-import itertools as its
-words = "1234567890abcdefghijklmnopqrstuvwxyz"
-numbers = "0123456789"
-r = its.product(words,repeat=8)
-dic = open("pwd.txt","a")
-for i in r:
-    dic.write("".join(i))
-    dic.write("".join("\n"))
-dic.close()
